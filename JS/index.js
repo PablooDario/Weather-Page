bf82eb0c05960ebc712ef10eaf55f33089e8423a
Pablo Sánchez <pabloosancheez0@gmail.com>
2023-06-06 23:32:44 -0600

    Add error parts

--- JS/index.js
+++ JS/index.js
@@ -29,14 +29,44 @@ buscar = () =>{
         let clima=ApiClima[index].skydescriptionlong
         let img="./img/" + imagen[clima]
         document.querySelector(".img-weather").src=img
+        document.querySelector(".meteor").style.display="none";
+        const icono = document.querySelector(".met");
+        if(icono!=null)
+        {
+            icono.className = " ";
+            icono.classList.add("fa-solid", "fa-cloud-showers-heavy", "fa-2xl", "item", "cloud");
+        }
     }
     else
-    alert("El municipio a buscar es: error")
+    {
+        alert("El municipio no existe")
+        document.querySelector(".meteor").style.display="block";
+        document.querySelector(".tmp").innerHTML="-180°C"
+        document.querySelector(".city").innerHTML="La Luna, Espacio"
+        document.querySelector(".precip").innerHTML="35%"
+        document.querySelector(".wind").innerHTML="7000km/h"
+        let img=document.querySelector(".img-weather")
+        img.src="./img/Moon.png"
+        img.style.height="180px";
+        img.style.width="180px";
+        document.querySelector(".imagen").style.position="relative";
+        document.querySelector(".imagen").style.top="10px";
+        document.querySelector(".principal").style.top="0px";
+        const icono = document.querySelector(".cloud");
+        icono.className = " ";
+        icono.classList.add("fa-solid", "fa-meteor", "fa-shake", "fa-2xl", "item", "met");
+    }
+    document.querySelector(".container").style.height="550px";
+    document.querySelector(".weather").style.display="block"
 }
 
 document.querySelector(".submit").addEventListener("click", buscar)
 
-
+document.addEventListener("keypress", (event)=>{
+    console.log(event);
+    if(document.querySelector("input").value.length>0 && event.key==="Enter")
+        buscar()
+})
 
 //Obtener el objeto que tenga el valor de "Asientos" en la propiedad "name"
 var result = ApiClima.find(municipio => municipio.name === "Asientos");
